4b7f42c863e747bf90f1f057666cd577ed57c3ec
Graham Perrin <grahamperrin@gmail.com>
2025-02-10 21:40:01 +0000

    FreeBSD-USB-Quick-Formatter.py: order and unmounting
    Alphabetical order for filesystems. 
    
    Reword, and enlarge, the hint about unmounting.

--- FreeBSD-USB-Quick-Formatter.py
+++ FreeBSD-USB-Quick-Formatter.py
@@ -25,14 +25,14 @@ def addDrives():
 def run():
     if drivesCombo.get() != "": #checks if a drive is selected
         drive = "/dev/" + drivesCombo.get()
-        if formatCombo.get() == "FAT32":
+        if formatCombo.get() == "Ext4":
+            formatcmd = "sudo gpart destroy -F " + drive + " ; " + "sudo gpart create -s gpt " + drive + " && " + "sudo gpart add -t linux-data " + drive + " && sleep 5 && " + "sudo mke2fs -t ext4 " + drive + "p1"
+        elif formatCombo.get() == "FAT32":
             formatcmd = "sudo gpart destroy -F " + drive + " ; " + "sudo gpart create -s mbr " + drive + " && " + "sudo gpart add -t fat32 " + drive + " && sleep 5 && " + "sudo newfs_msdos -F 32 " + drive + "s1"
-        elif formatCombo.get() == "UFS":
-            formatcmd = "sudo gpart destroy -F " + drive + " ; " + "sudo gpart create -s gpt " + drive + " && " + "sudo gpart add -t freebsd-ufs " + drive + " && sleep 5 && " + "sudo newfs " + drive + "p1"
         elif formatCombo.get() == "NTFS":
             formatcmd = "sudo gpart destroy -F " + drive + " ; " + "sudo gpart create -s mbr " + drive + " && " + "sudo gpart add -t ntfs " + drive + " && sleep 5 && " + "sudo mkntfs --quick " + drive + "s1"
-        elif formatCombo.get() == "Ext4":
-            formatcmd = "sudo gpart destroy -F " + drive + " ; " + "sudo gpart create -s gpt " + drive + " && " + "sudo gpart add -t linux-data " + drive + " && sleep 5 && " + "sudo mke2fs -t ext4 " + drive + "p1"
+        elif formatCombo.get() == "UFS":
+            formatcmd = "sudo gpart destroy -F " + drive + " ; " + "sudo gpart create -s gpt " + drive + " && " + "sudo gpart add -t freebsd-ufs " + drive + " && sleep 5 && " + "sudo newfs " + drive + "p1"
         cmd = "xterm -hold -e '" + formatcmd + " && echo Done. You can close the window." + "'"
         confirmation = tkinter.messagebox.askquestion("Confirmation", "Command: " + formatcmd)
         if confirmation == "yes":
@@ -54,14 +54,14 @@ drivesLabel = tkinter.Label(root, text="Drives:", font=("", 10))
 drivesCombo = ttk.Combobox(root, values=drives, state="readonly", font=("", 10))
 drivesButton = ttk.Button(root, command=driveInfo, text="Show Drives Info")
 formatLabel = tkinter.Label(root, text="Format:", font=("", 10))
-formatCombo = ttk.Combobox(root, values=("FAT32", "UFS", "NTFS", "Ext4"), state="readonly", font=("", 10))
+formatCombo = ttk.Combobox(root, values=("Ext4", "FAT32", "NTFS", "UFS"), state="readonly", font=("", 10))
 formatCombo.current(0)
 refreshButton = ttk.Button(root, command=refresh, text="Refresh")
 runButton = ttk.Button(root, command=run, text="Run")
 
 
 tkinter.Label(root, font=("", 6)).pack() #empty line workaround
-tkinter.Label(root, text="Note: You need to umount the drives first", font=("", 8)).pack()
+tkinter.Label(root, text="If the drive's filesystem is mounted: unmount it before proceeding.", font=("", 9)).pack()
 tkinter.Label(root, font=("", 8)).pack()
 drivesLabel.pack()
 drivesCombo.pack()
